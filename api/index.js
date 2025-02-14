4e7d14ba1ac60e8fdff0c2b66c79f0c6074a28c1
Babajide Ibiayo <babajideibiayo@yahoo.com>
2025-02-14 07:32:43 +0100

    modified vercel config

--- api/index.js
+++ api/index.js
@@ -2,12 +2,17 @@ import express, { json } from "express"
 import cors from "cors"
 import ShortUniqueId from "short-unique-id"
 import { createClient } from "redis"
+
 const redis = await createClient({ url: process.env.REDIS_URL })
   .on("error", (err) => console.log("Redis Client Error", err))
   .connect()
+
 const app = express()
 const port = process.env.PORT || 3003
-
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://dashboard-plum-kappa.vercel.app/"],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 app.use(cors())
 app.use(json())
 
@@ -49,7 +54,6 @@ app.post("/update-data", async (req, res) => {
           .status(404)
           .json({ success: false, message: "Data not found" })
       } else {
-        console.log("Data found")
         if (req.body.index === 0) {
           dtObj[index]["name"] = req.body.value
         } else if (req.body.index === 1) {
@@ -70,7 +74,6 @@ app.post("/update-data", async (req, res) => {
 })
 app.post("/delete-data", async (req, res) => {
   try {
-    let newJsonData = []
     const matchingKeys = await redis.keys("user*")
     let dt = matchingKeys.map((key) => redis.get(key))
     Promise.all([...dt]).then((values) => {
@@ -82,6 +85,7 @@ app.post("/delete-data", async (req, res) => {
           .status(404)
           .json({ success: false, message: "Data not found" })
       } else {
+        //console.log(matchingKeys[index])
         redis.del(matchingKeys[index])
         sendResponse(res, "Data deleted successfully")
       }
